31bdbbeaf3d8b71520511291b1a0ea8d92fc7db7
Emircvv7 <spike10lvl@gmail.com>
2023-09-15 17:23:48 +0600

    Initial commit

---
+++ Main.java
@@ -0,0 +1,32 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] array = {-7, -4, -2, 2, 3, 6, 8};
+        selectionSort(array);
+    }
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+            printArray(arr);
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
+
+
+
